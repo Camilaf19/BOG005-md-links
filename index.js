6fcf494f5bd2f72d2984429e797b000d2ebc8c7b
Camilaf19 <cam.fonseca19@gmail.com>
2022-10-24 11:07:39 -0500

    Se lee archivos md y directorio que contenga archivos md

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const path = require('path');
+
+const pathFile = 'goodDirectory';
+console.log(pathFile)
+
+const absolutPath = (pathFile) => {
+  let absolutPath;
+  if (path.isAbsolute(pathFile)) {
+    absolutPath = pathFile;
+  }
+  else {
+    absolutPath = path.resolve(pathFile);
+  }
+  return absolutPath
+}
+console.log(absolutPath(pathFile))
+
+
+function getFiles(pathFile) {
+  const realPath = absolutPath(pathFile)
+  let arrayPaths = [];
+  //si es un archivo
+  if (fs.statSync(realPath).isFile() === true && path.extname(realPath) === '.md') {
+    arrayPaths.push(realPath);
+  } else if (fs.statSync(realPath).isFile() && path.extname(realPath) !== '.md') {
+    console.log("No es un archivo md")
+  } 
+  else { //si es un directorio
+    fs.readdirSync(realPath).forEach(file => {
+      let pathDirectory = path.join(realPath, file);
+      if (fs.statSync(realPath).isDirectory() === true) {
+        console.log(file, "aqui lo recorrió")
+        if (path.extname(pathDirectory) === ".md") {
+          arrayPaths.push(pathDirectory);
+        }
+      }
+    })   
+  }
+  return arrayPaths;
+}
+console.log(getFiles(pathFile));
+
+module.exports = {
+
+}
---
+++ md-links.js
@@ -0,0 +1,5 @@
+/* const mdLinks = (path, options) => {
+    return new Promise((resolve, reject) => {
+  new Error('No es una ruta válida');
+  }
+  mdLinks(path) */
